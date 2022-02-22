b782c121b61febc524e93fa2e5ba72d7f632885e
juansoteldo <jsoteldoup@gmail.com>
2022-02-22 15:15:33 -0600

    Update APP Key

--- src/helpers/api/config.js
+++ src/helpers/api/config.js
@@ -4,7 +4,7 @@ const APIConfig = (methodType, endpoint, data) => {
     method: methodType,
     url: `${process.env.React_App_BASE_URL}${endpoint}`,
     headers: {
-      'APP_KEY': `${process.env.React_App_ACCESS_TOKEN}`  
+      'APP-KEY': `${process.env.React_App_ACCESS_TOKEN}`  
     },
     data: data
   };
