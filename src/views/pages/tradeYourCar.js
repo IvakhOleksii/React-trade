df354f81b0aabf88581110280f4b1c6b0dd5f832
juansoteldo <jsoteldoup@gmail.com>
2022-02-23 13:11:30 -0600

    Created single component for Trade and Sell

--- src/views/pages/tradeYourCar.js
+++ src/views/pages/tradeYourCar.js
@@ -1,42 +1,58 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import GetRegistered from "../../components/getRegistered";
+import Footer from "../_partials/footer";
+import YourCarForm, {
+  FORM_MODES,
+} from "../../components/YourCarForm/YourCarForm";
 
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import GetRegistered from '../../components/getRegistered';
-import Footer from '../_partials/footer';
-import TradeYourCarHero from '../../components/tradeYourCar hero/tradeYourCarHero';
-// import Loader from '../../components/loader';
+const CARDS_DATA = {
+  heading1: "Tell Us Where You are in the",
+  heading2: "Purchasing Process.",
+  cards: [
+    {
+      value:
+        "  I’m ready to purchase now. Get me Top Dollar for my Trade-in Vehicle.",
+    },
+    {
+      value:
+        "  I’m within 60 days of a purchase. Get me Top Dollar for my Trade-in Vehicle.",
+    },
+    {
+      value:
+        " I’m not quite ready, I’d like an idea of what to expect for my Trade-in Vehicle.",
+    },
+  ],
+};
+
 class TradeYourCar extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      loading: true
-    } 
+      loading: true,
+    };
   }
- 
+
   render() {
     return (
-      // <React.Fragment>
-      //   {this.state.loading ?
-      //     <Loader />
-      //     :
-          <React.Fragment>
-            <TradeYourCarHero {...this.props} />
-            <GetRegistered {...this.props} />
-            <Footer {...this.props} />
-          </React.Fragment>
-      //     }
-      // </React.Fragment>
-    )
+      <React.Fragment>
+        <YourCarForm
+          {...this.props}
+          mode={FORM_MODES.TRADE}
+          cardsData={CARDS_DATA}
+        />
+        <GetRegistered {...this.props} />
+        <Footer {...this.props} />
+      </React.Fragment>
+    );
   }
 }
 const mapStateToProps = (state) => {
   return {
-    vouched: state.app.vouched
+    vouched: state.app.vouched,
   };
 };
 const mapDispatchToProps = (dispatch) => {
-  return {
-
-  };
+  return {};
 };
 export default connect(mapStateToProps, mapDispatchToProps)(TradeYourCar);
